7719f752611081291c3cee6cba19a794d583a921
JackMarx <seriousfools@gmail.com>
2017-08-30 20:18:42 -0500

    url example

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -3,4 +3,9 @@ class ParamsController < ApplicationController
     @message = params[:my_message]
     @message_2 = params[:my_message_2]
   end
+
+  def url_example
+    @value = params[:this_is_a_key_variable]
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/query_string_example' => 'params#query_example'
+  get '/url_segment_example/:this_is_a_key_variable' => 'params#url_example'
 end
